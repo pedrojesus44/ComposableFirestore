63b3d8c412476d239b87d8af8ac683a6e294bd59
pedrojesus44 <kolibuoficial@gmail.com>
2023-09-06 09:04:35 -0300

    tela

--- app/src/main/java/com/example/composable/MainActivity.kt
+++ app/src/main/java/com/example/composable/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.composable
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,9 +17,13 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -59,7 +67,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
@@ -71,6 +81,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.composable.ui.theme.ComposableTheme
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -253,18 +264,43 @@ fun LoginForm() {
         )
         EditableExposedDropdownMenuSample()
 
-        OutlinedTextField(
-            value = data,
-            onValueChange = { data = it },
-            label = { Text(text = "Data") },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                imeAction = ImeAction.Done,
-                keyboardType = KeyboardType.Text
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
+        val context = LocalContext.current
+        val calendar = Calendar.getInstance()
+        var selectedDateText by remember { mutableStateOf("") }
+        val year = calendar[Calendar.YEAR]
+        val month = calendar[Calendar.MONTH]
+        val dayOfMonth = calendar[Calendar.DAY_OF_MONTH]
+        val datePicker = DatePickerDialog(
+            context,
+            { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDayOfMonth: Int ->
+                selectedDateText = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+            }, year, month, dayOfMonth
         )
+        datePicker.datePicker.minDate = calendar.timeInMillis
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    datePicker.show()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(Color.White)
+                    .clip(shape = CircleShape)
+            ) {
+                Text(
+                    text = if (selectedDateText.isNotEmpty()) {
+                        "Data Selecionada $selectedDateText"
+                    } else {
+                        "Data de Contato"
+                    }
+                )
+            }
+        }
 
         OutlinedTextField(
             value = observacao,
@@ -276,7 +312,8 @@ fun LoginForm() {
             ),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
+                .height(100.dp)
+                .padding(10.dp)
         )
 
         Row(
@@ -314,6 +351,10 @@ fun EditableExposedDropdownMenuSample() {
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = { expanded = !expanded },
+        modifier = Modifier
+            .width(1020.dp)
+            .background(Color.White)
+            .clip(shape = CircleShape)
     ) {
         TextField(
             // The `menuAnchor` modifier must be passed to the text field for correctness.
